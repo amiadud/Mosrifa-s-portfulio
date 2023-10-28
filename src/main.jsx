18f39cd572b72b9896a0c029fe3ce4f3efbc9025
csesopnil <sopnilstar@gmail.com>
2023-10-28 23:39:45 +0600

    added

--- src/main.jsx
+++ src/main.jsx
@@ -7,24 +7,12 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import About from './components/About/About.jsx';
-import Contact from './components/Contact/Contact.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
-    children:[
-      {
-        path: "/about",
-        element:<About/>
-      },
-      {
-        path: "/contact",
-        element:<Contact/>
-      }
-    ]
-  },
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
